9345d3d01cdf6ffb92df4abdb82c368139c6eb10
JainamShah3008 <jainams597@gmail.com>
2025-08-07 20:07:53 +0530

    Add : add route

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const userRoute = require("./user");
+const orderRoute = require("./order");
 
 router.use("/user", userRoute);
+router.use("/order", orderRoute);
 
 module.exports = router;
